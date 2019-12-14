019025a144305fb3030285db9812308af61dd63a
otumian <popecan1000@gmail.com>
2019-12-14 16:46:01 +0000

    pill_code_db - added the schema and the database similar to the php version

---
+++ pill_code_db.sql
@@ -0,0 +1,33 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS `users` (
+	`user_id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`user_first_name`	TEXT NOT NULL,
+	`user_last_name`	TEXT NOT NULL,
+	`user_email`	TEXT NOT NULL UNIQUE,
+	`user_password`	TEXT NOT NULL,
+	`user_bio`	INTEGER NOT NULL,
+	`user_register_date`	INTEGER NOT NULL
+);
+CREATE TABLE IF NOT EXISTS `tokens` (
+	`token_id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`token_text`	TEXT NOT NULL UNIQUE,
+	`token_date`	TEXT NOT NULL,
+	`token_dormancy`	TEXT NOT NULL,
+	`token_purpose`	TEXT NOT NULL,
+	`user_email`	TEXT NOT NULL
+);
+CREATE TABLE IF NOT EXISTS `comments` (
+	`comment_id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`post_id`	INTEGER NOT NULL,
+	`comment_text`	TEXT NOT NULL,
+	`comment_date`	TEXT NOT NULL,
+	`user_email`	TEXT NOT NULL
+);
+CREATE TABLE IF NOT EXISTS `articles` (
+	`post_id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`post_title`	TEXT NOT NULL,
+	`post_content`	TEXT NOT NULL,
+	`post_date`	TEXT NOT NULL,
+	`user_email`	TEXT NOT NULL
+);
+COMMIT;
